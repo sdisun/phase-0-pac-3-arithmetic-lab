ccc5d1f5ab843c661ec1c014eed8c0b85ad48067
Neil <zooming.in@hotmail.com>
2021-03-16 22:31:18 +1100

    Initialize git

---
+++ index.js
@@ -0,0 +1,39 @@
+function add (num1, num2) {
+    let res = num1 + num2
+    return res
+}
+
+function subtract(num1, num2) {
+    let res = num1 - num2
+    return res
+}
+
+function multiply(num1, num2) {
+    let res = num1 * num2
+    return res
+}
+
+function divide(num1, num2) {
+    let res = num1 / num2
+    return res
+}
+
+function increment(n) {
+    let res = n++;
+    return n;
+}
+
+function decrement(n) {
+    let res = n--;
+    return n;
+}
+
+function makeInt(n) {
+    let res = parseInt(n, 10);
+    return n;
+}
+
+function preserveDecimal(n) {
+    let res = parseFloat(n);
+    return n;
+}
